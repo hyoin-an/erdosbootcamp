4ebf9b3c8a4f1ac55f584c3c38113fde12ce79f7
JonghooLee <jonghoo.lee13@gmail.com>
2023-11-05 21:51:34 -0500

    movies_df added

--- tmdb.py
+++ tmdb.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 
+
 url = 'https://api.themoviedb.org/3/'
 
 key = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGJkYWFmYTU3MjNhOGRkMmNjNjhlNDVjNjgyNjRiYiIsInN1YiI6IjY1NDgyZmRiNmJlYWVhMDEyYzhmMGJkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tam6Qgp_P3HxfAEVUmLD2kl3NJYS0am3zXRSChVfmFM'
@@ -38,26 +39,29 @@ class Tmdb():
     
     def movie_details(self, id : int, full_details = False):
         """
-        returns json format of details of the movie corresponding to the given id
+        returns a pd.Series consisting of the movie corresponding to the given id
 
         id : movie id
 
         full_details : default is False. When = False, only returns relevant data.
         """
         url = f'https://api.themoviedb.org/3/movie/{id}?language=en-US"'
-      
+
         headers = {
                 "accept": "application/json",
                 "Authorization": self.key
-        }
+            }
         
         response = requests.get(url, headers=headers).json()
-        response = pd.Series(response)
-      
-        short = response.drop(['backdrop_path','belongs_to_collection','homepage','imdb_id','id','original_language','overview','poster_path','production_companies','production_countries','status','tagline','title','video'])
+        response = pd.Series(response).drop(['backdrop_path', 'adult'])
+        
 
-        short['genres'] = short['genres'][0]['name']
-        short['spoken_languages'] = short['spoken_languages'][0]['english_name']
+        ## drop unnecessary features when full_details = False
+        short = response.drop(['homepage','imdb_id','id','original_language','overview','poster_path','production_companies','production_countries','status','tagline','title','video'])
+        
+        #short['genres'] = short['genres'][0]['name']
+        #short['spoken_languages'] = short['spoken_languages'][0]['english_name']
+
         if full_details:
             return response
         else:
@@ -69,13 +73,15 @@ class Tmdb():
         df = pd.DataFrame(columns=columns)
 
         id_list = self.movie_id(page, category)
-
-        for idx, id in enumerate(id_list):
-            df[f'{idx}'] = self.movie_details(id, full_details)
-
+        
+        movie_series = [self.movie_details(id) for id in id_list]
+        
+        df = pd.concat(movie_series, axis=1)
         return df.transpose()
+    
+    def movies_df(self, num = 19, category ='top_rated', full_details = False):
+        page = num //19
+        return self.movie_df(page, category = category, full_details=full_details)
 
-
-tm = Tmdb(key)
-
-print(tm.movie_df(2))
+# tm = Tmdb(key)
+# print(tm.movies_df(300))
